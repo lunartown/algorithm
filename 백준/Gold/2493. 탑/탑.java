bdfb613c40064c02db7dcbe0170a6d0c1560c24d
lunartown <99635477+lunartown@users.noreply.github.com>
2023-02-13 16:35:02 +0900

    [Gold V] Title: 탑, Time: 576 ms, Memory: 106404 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,21 +10,21 @@ public class Main {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		Stack<Integer[]> tower = new Stack<>();
+		ArrayDeque<int[]> tower = new ArrayDeque<>();
 		int N = Integer.parseInt(in.readLine());
 		int hgt = 0;
 		
 		st = new StringTokenizer(in.readLine());
 		
-		tower.push(new Integer[] {0, 0});
+		tower.push(new int[] {0, 0});
 		
 		for(int i = 1; i <= N; i++) {
 			hgt = Integer.parseInt(st.nextToken());
 			while(tower.peek()[0] != 0 && tower.peek()[1] < hgt) {
 				tower.pop();
 			}
-			sb.append(tower.peek()[0] + " ");
-			tower.push(new Integer[] {i, hgt});
+			sb.append(tower.peek()[0]).append(" ");
+			tower.push(new int[] {i, hgt});
 		}
 		
 		System.out.println(sb);
