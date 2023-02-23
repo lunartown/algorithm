de9f16182f7d6903b4542278dc362c8002a074ce
lunartown <99635477+lunartown@users.noreply.github.com>
2023-02-23 22:53:49 +0900

    [Gold IV] Title: 알파벳, Time: 468 ms, Memory: 12416 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,27 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 	
 public class Main {
 	static int R, C, max = 1;
-	static int[] dx = {0, 1, 0, -1};
-	static int[] dy = {1, 0, -1, 0};
 	
 	private static void search(int curX, int curY, int len, int alpha, char[][] board, int[][] vis) {		
 		max = Math.max(max, len);
 		if(max == 26) return;
+		int nextAlpha, nx, ny;
 		
-		for(int i = 0; i < 4; i++) {
-			int nx = curX + dx[i];
-			int ny = curY + dy[i];
-			if((alpha & (1 << (board[nx][ny] - 'A'))) != 0
-				|| (alpha | (1 << (board[nx][ny] - 'A'))) == vis[nx][ny]) continue;
+		if(curX > 0) {
+			nx = curX - 1;
+			nextAlpha = 1 << (board[nx][curY] - 'A');
 			
-			vis[nx][ny] = alpha;
-			search(nx, ny, len + 1, alpha | (1 << (board[nx][ny] - 'A')), board, vis);
+			if((alpha & nextAlpha) == 0
+					&& (alpha | nextAlpha) != vis[nx][curY]) {
+				vis[nx][curY] = alpha;
+				search(nx, curY, len + 1, alpha | (1 << (board[nx][curY] - 'A')), board, vis);
+			}
 		}
+		
+		if(curX < R - 1) {
+			nx = curX + 1;
+			nextAlpha = 1 << (board[nx][curY] - 'A');
+			
+			if((alpha & nextAlpha) == 0
+					&& (alpha | nextAlpha) != vis[nx][curY]) {
+				vis[nx][curY] = alpha;
+				search(nx, curY, len + 1, alpha | (1 << (board[nx][curY] - 'A')), board, vis);
+			}
+		}
+		
+		if(curY > 0) {
+			ny = curY - 1;
+			nextAlpha = 1 << (board[curX][ny] - 'A');
+			
+			if((alpha & nextAlpha) == 0
+					&& (alpha | nextAlpha) != vis[curX][ny]) {
+				vis[curX][ny] = alpha;
+				search(curX, ny, len + 1, alpha | (1 << (board[curX][ny] - 'A')), board, vis);
+			}
+		}
+		
+		if(curY < C - 1) {
+			ny = curY + 1;
+			nextAlpha = 1 << (board[curX][ny] - 'A');
+			
+			if((alpha & nextAlpha) == 0
+					&& (alpha | nextAlpha) != vis[curX][ny]) {
+				vis[curX][ny] = alpha;
+				search(curX, ny, len + 1, alpha | (1 << (board[curX][ny] - 'A')), board, vis);
+			}
+		}
+		
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -31,28 +64,19 @@ public class Main {
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		
-		char[][] board = new char[R+2][C+2];
-		int[][] vis = new int[R+1][C+1];
+		char[][] board = new char[R][C];
+		int[][] vis = new int[R][C];
 		String str;
 		
-		for(int i = 1; i <= R; i++) {
+		for(int i = 0; i < R; i++) {
 			str = in.readLine();
-			for(int j = 1; j <= C; j++) {
-				board[i][j] = str.charAt(j-1);
+			for(int j = 0; j < C; j++) {
+				board[i][j] = str.charAt(j);
 			}
 		}
 		
-		char wall = board[1][1];
-        Arrays.fill(board[0], wall);
-        Arrays.fill(board[R + 1], wall);
-        for (int i = 0; i <= R + 1; i++){
-            for (int j : new int[]{0, C+1}){
-                board[i][j] = wall;
-            }
-        }
-		
 		int alpha = 1 << (board[0][0] - 'A');
-		search(1, 1, 1, alpha, board, vis);
+		search(0, 0, 1, alpha, board, vis);
 		System.out.println(max);
 	}
 }
