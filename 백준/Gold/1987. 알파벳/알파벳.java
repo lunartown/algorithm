9d7d4b756223247a36e005e994405dd349a3b1eb
lunartown <99635477+lunartown@users.noreply.github.com>
2023-02-23 22:36:22 +0900

    [Gold IV] Title: 알파벳, Time: 836 ms, Memory: 12500 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -4,6 +4,25 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 	
 public class Main {
+	static int R, C, max = 1;
+	static int[] dx = {0, 1, 0, -1};
+	static int[] dy = {1, 0, -1, 0};
+	
+	private static void search(int curX, int curY, int len, int alpha, char[][] board, int[][] vis) {		
+		max = Math.max(max, len);
+		if(max == 26) return;
+		
+		for(int i = 0; i < 4; i++) {
+			int nx = curX + dx[i];
+			int ny = curY + dy[i];
+			if(nx < 0 || nx >= R || ny < 0 || ny >= C
+				|| (alpha & (1 << (board[nx][ny] - 'A'))) != 0
+				|| (alpha | (1 << (board[nx][ny] - 'A'))) == vis[nx][ny]) continue;
+			
+			vis[nx][ny] = alpha;
+			search(nx, ny, len + 1, alpha | (1 << (board[nx][ny] - 'A')), board, vis);
+		}
+	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -13,8 +32,7 @@ public class Main {
 		C = Integer.parseInt(st.nextToken());
 		
 		char[][] board = new char[R][C];
-		boolean[][] vis = new boolean[R][C];
-		boolean[] alpha = new boolean[26];
+		int[][] vis = new int[R][C];
 		String str;
 		
 		for(int i = 0; i < R; i++) {
@@ -24,30 +42,8 @@ public class Main {
 			}
 		}
 		
-		alpha[board[0][0] - 'A'] = true;
+		int alpha = 1 << (board[0][0] - 'A');
 		search(0, 0, 1, alpha, board, vis);
 		System.out.println(max);
-	}
-
-	static int R, C, max = 1;
-	static int[] dx = {0, 1, 0, -1};
-	static int[] dy = {1, 0, -1, 0};
-	
-	private static void search(int curX, int curY, int len, boolean[] alpha, char[][] board, boolean[][] vis) {		
-		for(int i = 0; i < 4; i++) {
-			int nx = curX + dx[i];
-			int ny = curY + dy[i];
-			if(nx < 0 || nx >= R || ny < 0 || ny >= C
-					|| vis[nx][ny]
-							|| alpha[board[nx][ny] - 'A']) continue;
-			
-			vis[nx][ny] = true;
-			alpha[board[nx][ny] - 'A'] = true;
-			search(nx, ny, len + 1, alpha, board, vis);
-			vis[nx][ny] = false;
-			alpha[board[nx][ny] - 'A'] = false;
-		}
-		
-		max = Math.max(max, len);
 	}
 }
