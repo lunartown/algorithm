efd39e4861a6cc1adb5f5e0c1c94ed25cea0d7bc
lunartown <99635477+lunartown@users.noreply.github.com>
2024-07-18 12:13:27 +0900

    [Silver III] Title: 칸토어 집합, Time: 188 ms, Memory: 29752 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -1,16 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Main {
-    public static StringBuilder Cantor (int N) {
-        if(N == 0) return new StringBuilder("-");
+    private static Map<Integer, String> map = new HashMap<>();
+
+    public static String Cantor (int N) {
+        if(N == 0) return "-";
+        if(map.containsKey(N)) return map.get(N);
+
         StringBuilder empty = new StringBuilder();
         for(int i = 0; i < Math.pow(3, N - 1); i++) {
             empty.append(" ");
         }
-        return Cantor(N - 1).append(empty).append(Cantor(N - 1));
+
+        String ans = Cantor(N - 1) + empty.toString() + Cantor(N - 1);
+        map.put(N, ans);
+        return ans;
     }
 
     public static void main(String[] args) throws IOException {
