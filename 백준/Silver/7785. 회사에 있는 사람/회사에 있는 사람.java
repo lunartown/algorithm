eea9a195ad6cc69def7e69e38ee4f84810d69a2c
lunartown <99635477+lunartown@users.noreply.github.com>
2024-07-19 12:46:40 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 684 ms, Memory: 52716 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -1,34 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 //출근한 사람 세기
 //enter하고 leave 안 하면 출근자
-//set에 넣어었다 뺐다 하자
+//tree에 넣어서 마지막 입력만 받자
+//TreeMap에 Comparator써서 자동으로 정렬하자
+//EntrySet으로 출력
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
-        Set<String> set = new HashSet<String>();
+
+        TreeMap<String, String> map = new TreeMap<>((a, b)->
+                b.compareTo(a));
 
         int N = Integer.parseInt(br.readLine());
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            String name = st.nextToken();
-            String log = st.nextToken();
-
-            if(log.equals("enter")) {
-                set.add(name);
-            }else if(log.equals("leave")) {
-                set.remove(name);
-            }
+            map.put(st.nextToken(), st.nextToken());
         }
 
-        set.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+        map.entrySet().stream().filter((entry) -> entry.getValue().equals("enter"))
+                .forEach((entry) -> sb.append(entry.getKey()).append("\n"));
+
+        System.out.println(sb);
     }
 }
