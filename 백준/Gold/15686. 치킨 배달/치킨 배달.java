4694f86267d204af0599b134382e0439dcf2f727
lunartown <99635477+lunartown@users.noreply.github.com>
2024-07-23 11:41:25 +0900

    [Gold V] Title: 치킨 배달, Time: 156 ms, Memory: 15424 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,73 +1,88 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
+// 치킨집 살리기 운동
+// 1. combination으로 살아있는 치킨집 조합을 모두 구한다.
+// 2. map이 주어졌을 때 도시의 치킨거리를 구하는 메소드를 작성한다.
+// 2-1. 치킨거리는 각 집의 치킨거리의 합
+// 3. min 밸류를 가지고 실패 시 백트래킹한다.
 
 public class Main {
+    private static int N, M;
+    private static int[][] city;
+    private static int[][] remain;
+    private static List<int[]> houses = new ArrayList<>();
+    private static List<int[]> chickens = new ArrayList<>();
+    private static int minChicken = Integer.MAX_VALUE;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(in.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		int[][] city = new int[N+1][N+1];
-		List<int[]> house = new ArrayList<>();
-		List<int[]> chicken = new ArrayList<>();
-		List<int[]> newChicken = new ArrayList<>();
-		
-//		도시 입력받기
-//		집, 치킨집의 위치 리스트에 저장
-		for(int i = 1; i <= N; i++) {
-			st = new StringTokenizer(in.readLine());
-			for(int j = 1; j <= N; j++) {
-				city[i][j] = Integer.parseInt(st.nextToken());
-				if(city[i][j] == 1) house.add(new int[] {i, j});
-				else if(city[i][j] == 2) chicken.add(new int[] {i, j});
-			}
-		}
-		
-		combination(house, chicken, newChicken, M, 0, -1);
-		System.out.println(min);
-		
-	}
-	
-	static int min = Integer.MAX_VALUE;
-	
-	static void combination(List<int[]> house, List<int[]> chicken, List<int[]> newChicken, int M, int cnt, int flag) {
-		if(newChicken.size() >= M) {
-			min = Math.min(min, chickenDis(house, newChicken));
-			return;
-		}
-		
-		for(int i = 0; i < chicken.size(); i++) {
-			if(i <= flag) continue;
-			newChicken.add(chicken.get(i));
-			combination(house, chicken, newChicken, M, cnt+1, i);
-			newChicken.remove(newChicken.size() - 1);
-		}
-	}
-	
-	static int chickenDis(List<int[]> house, List<int[]> chicken) {
-		int dis, disCity = 0;
-		for(int i = 0; i < house.size(); i++) {
-			dis = Integer.MAX_VALUE;
-			for(int j = 0; j < chicken.size(); j++) {
-				dis = Math.min(dis, distance(house.get(i), chicken.get(j)));
-			}
-			disCity += dis;
-		}
-		return disCity;
-	}
-	
-	static int distance(int[] house, int[] chicken) {
-		return (Math.abs(house[0] - chicken[0]) + Math.abs(house[1] - chicken[1]));
-	}
-	
+    private static void combination(int depth, int idx) {
+        if (depth == M) {
+            //치킨거리 구하는 공식
+            getCityChickenLength();
+            return;
+        }
+
+        if (idx == chickens.size()) {
+            return;
+        }
+
+        remain[depth] = chickens.get(idx).clone();
+        combination(depth + 1, idx + 1);
+        combination(depth, idx + 1);
+    }
+
+    private static void getCityChickenLength() {
+        int sum = 0;
+        int housesNum = houses.size();
+
+        for (int i = 0; i < housesNum; i++) {
+            int[] house = houses.get(i);
+            //최대 거리
+            int min = (N - 1) * 2;
+            for (int[] chicken : remain) {
+                int length = getChickenLength(house, chicken);
+                if (length < min) min = length;
+            }
+            sum += min;
+            if (sum + (housesNum - i - 1) >= minChicken) {
+                return;
+            }
+        }
+
+        if (sum < minChicken) {
+            minChicken = sum;
+        }
+    }
+
+    private static int getChickenLength(int[] h1, int[] h2) {
+        return Math.abs(h1[0] - h2[0]) + Math.abs(h1[1] - h2[1]);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        city = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                city[i][j] = Integer.parseInt(st.nextToken());
+                if (city[i][j] == 1) {
+                    houses.add(new int[]{i, j});
+                } else if (city[i][j] == 2) {
+                    chickens.add(new int[]{i, j});
+                }
+            }
+        }
+
+        remain = new int[M][];
+        combination(0, 0);
+        System.out.println(minChicken);
+    }
 }
