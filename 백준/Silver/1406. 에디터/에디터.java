dcdb51f3df70e66959816c8cc10e043e061c8a57
lunartown <99635477+lunartown@users.noreply.github.com>
2024-07-24 17:35:29 +0900

    [Silver II] Title: 에디터, Time: 664 ms, Memory: 177504 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,43 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
 import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		String str = in.readLine();
-		LinkedList<Character> write = new LinkedList<Character>();
-		for(int i = 0; i < str.length(); i++) {
-			write.add(str.charAt(i));
-		}
-		
-		ListIterator<Character> iterator = write.listIterator(str.length());
-		
-		int N = Integer.parseInt(in.readLine());
-		char cmd;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        LinkedList<Character> string = br.readLine().chars()
+                .mapToObj(i -> (char) i)
+                .collect(Collectors.toCollection(LinkedList::new));
 
-		for(int i = 0; i < N; i++) {
-			st = new StringTokenizer(in.readLine());
-			cmd = st.nextToken().charAt(0);
-			switch(cmd) {
-			case 'L' : if(iterator.hasPrevious()) iterator.previous(); break;
-			case 'D' : if(iterator.hasNext()) iterator.next(); break;
-			case 'B' : if(iterator.hasPrevious()) { iterator.previous(); iterator.remove(); } break;
-			case 'P' : iterator.add(st.nextToken().charAt(0));
-			}
-		}
-		
-		ListIterator<Character> printer = write.listIterator();
-		
-		while(printer.hasNext()) {
-			sb.append(printer.next());
-		}
-		
-		System.out.println(sb);
-	}
+        ListIterator<Character> iterator = string.listIterator(string.size());
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            switch(st.nextToken()) {
+                case "L":
+                    if(iterator.hasPrevious()) {
+                        iterator.previous();
+                    }
+                    break;
+                case "D":
+                    if(iterator.hasNext()) {
+                        iterator.next();
+                    }
+                    break;
+                case "B":
+                    if(iterator.hasPrevious()) {
+                        iterator.previous();
+                        iterator.remove();
+
+                    }
+                    break;
+                case "P":
+                    iterator.add(st.nextToken().charAt(0));
+                    break;
+                default:
+                    throw new IOException("Invalid input");
+            }
+        }
+        System.out.println(string.stream()
+                .map(i -> i.toString())
+                .collect(Collectors.joining()));
+    }
 }
