4e026082aa4efa48f1ab8c0e4c2859df59007344
lunartown <99635477+lunartown@users.noreply.github.com>
2024-07-25 14:23:47 +0900

    [Gold III] Title: 두 배열의 합, Time: 616 ms, Memory: 31652 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
@@ -13,6 +13,27 @@ import java.util.stream.Stream;
 //4. 더한다.
 //1000 x 1_000_000이니까 int 안에 다 들어올 듯?
 public class Main {
+    private static int findValue(int[] arr, long value) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            if(low == high && arr[high] == value)
+                return high;
+
+            int mid = (low + high + 1) >>> 1;
+            if (arr[mid] < value) {
+                low = mid + 1;
+            } else if (arr[mid] > value) {
+                high = mid - 1;
+            } else {
+                low = mid;
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -27,17 +48,17 @@ public class Main {
         int[] subB = new int[m * (m + 1) / 2];
 
         int idx = 0;
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             int sum = 0;
-            for(int j = i; j < n; j++) {
+            for (int j = i; j < n; j++) {
                 subA[idx++] = sum += A[j];
             }
         }
 
         idx = 0;
-        for(int i = 0; i < m; i++) {
+        for (int i = 0; i < m; i++) {
             int sum = 0;
-            for(int j = i; j < m; j++) {
+            for (int j = i; j < m; j++) {
                 subB[idx++] = sum += B[j];
             }
         }
@@ -45,29 +66,24 @@ public class Main {
         Arrays.sort(subA);
         Arrays.sort(subB);
 
-        //투 포인터
-        int left = 0;
-        int right = subB.length - 1;
+        //이진 탐색
         long count = 0;
-        while(left < subA.length && right >= 0) {
-            long sumA = subA[left];
-            long sumB = subB[right];
-            long sum = sumA + sumB;
+        for (int i = 0; i < subA.length;) {
+            long sumA = subA[i];
+            long sumB = T - sumA;
 
-            if(sum > T)
-                right--;
-            else if (sum < T)
-                left++;
-            else if (sum == T) {
-                long duplA = 0;
-                long duplB = 0;
-                for(;left < subA.length && (subA[left] == sumA); left++)
-                    duplA++;
-                for(;right >= 0 && (subB[right] == sumB); right--)
-                    duplB++;
-
-                count += duplA * duplB;
+            long duplB = 0;
+            int bUpper = findValue(subB, sumB);
+            for (int j = bUpper; j >= 0 && subB[j] == sumB; j--) {
+                duplB++;
             }
+
+            long duplA = 0;
+            for (; i < subA.length && subA[i] == sumA; i++) {
+                duplA++;
+            }
+
+            count += duplA * duplB;
         }
 
         System.out.println(count);
