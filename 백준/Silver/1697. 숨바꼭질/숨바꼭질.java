75bb03da8b8c35c3e00fbffdd9d054de72e52d92
lunartown <99635477+lunartown@users.noreply.github.com>
2024-07-30 09:15:18 +0900

    [Silver I] Title: 숨바꼭질, Time: 124 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,48 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String args[]) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(in.readLine());
-		Queue<Integer> bfs = new LinkedList<>();
-		int[] road = new int[100001];
-		
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		road[N] = 1;
-		int time = 0;
-		int cur = 0;
-		bfs.add(N);
-		
-		label:while(!bfs.isEmpty()) {
-			int size = bfs.size();
-			for(int i = 0; i < size; i++) {
-			cur = bfs.poll(); 
-//			System.out.println(time + " cur = " + cur);
-				if(cur == K) break label;
-				for(int j = 1; j <= 3; j++) {
-					switch(j) {
-					case 1: if(cur-1 < 0 || road[cur-1] != 0) continue;
-					bfs.add(cur-1);
-					road[cur-1] = 1; break;
-					case 2: if(cur+1 > 100000 || road[cur+1] != 0) continue;
-					bfs.add(cur+1);
-					road[cur+1] = 1; break;
-					case 3: if(cur*2 > 100000 || road[cur*2] != 0) continue;
-					bfs.add(cur*2);
-					road[cur*2] = 1;
-					}
-				}
-			}
-			time++;
-		}
-		System.out.println(time);
-	}
+    static final int MAX = Integer.MAX_VALUE;
+    static final int RANGE = 100_000;
+
+    static boolean isMovable(int i) {
+        if (i < 0 || i > RANGE) return false;
+        return true;
+    }
+
+    static int nextMove(int i, int dir) {
+        if (dir == 0) return i - 1;
+        if (dir == 1) return i + 1;
+        if (dir == 2) return i * 2;
+        else return 0;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        //거리(시간) 배열
+        int[] dis = new int[RANGE + 1];
+        //bfs
+        Queue<Integer> queue = new ArrayDeque<>();
+        //초기값 무한대
+        Arrays.fill(dis, MAX);
+        dis[N] = 0;
+        queue.offer(N);
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            if (cur == K)
+                break;
+
+            for (int i = 0; i < 3; i++) {
+                int next = nextMove(cur, i);
+                if (!isMovable(next))
+                    continue;
+                //갱신되는 상황에만
+                if (dis[cur] + 1 < dis[next]) {
+                    dis[next] = dis[cur] + 1;
+                    queue.offer(next);
+                }
+            }
+        }
+
+        System.out.println(dis[K]);
+    }
 }
