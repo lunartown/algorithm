19593a95135f8b192e9e66e3a8789f364c945904
lunartown <99635477+lunartown@users.noreply.github.com>
2024-09-01 08:55:22 +0900

    [Gold V] Title: 두 용액, Time: 384 ms, Memory: 37740 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,10 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 //두 용액
 //https://www.acmicpc.net/problem/2470
@@ -17,57 +15,37 @@ public class Main {
         //용액의 수
         int N = Integer.parseInt(br.readLine());
 
-        //long 타입으로 선언
-        List<Long> positive = new ArrayList<>();
-        List<Long> negative = new ArrayList<>();
+        //배열
+        int[] arr = Stream.of(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
-        //용액 입력
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            long num = Long.parseLong(st.nextToken());
-            if (num > 0) {
-                positive.add(num);
-            } else {
-                negative.add(num);
-            }
-        }
-
-        //양수, 음수 정렬
-        positive.sort(null);
-        negative.sort(Comparator.comparing(Long::longValue).reversed());
+        //정렬
+        Arrays.sort(arr);
 
         //투 포인터
-        long min = Long.MAX_VALUE;
-        int pIdx = 0;
-        int nIdx = 0;
+        int left = 0;
+        int right = N - 1;
+        int min = Integer.MAX_VALUE;
+        int[] answer = new int[2];
 
-        for (int p = 0, n = 0; p < positive.size() && n < negative.size(); ) {
-            long sum = positive.get(p) + negative.get(n);
+        while (left < right) {
+            int sum = arr[left] + arr[right];
 
-            if (Math.abs(sum) < Math.abs(min)) {
-                min = sum;
-                pIdx = p;
-                nIdx = n;
+            if (Math.abs(sum) < min) {
+                min = Math.abs(sum);
+                answer[0] = arr[left];
+                answer[1] = arr[right];
             }
 
             if (sum < 0) {
-                p++;
+                left++;
             } else {
-                n++;
+                right--;
             }
         }
 
-        long posMin = positive.size() >= 2 ? positive.get(0) + positive.get(1) : Long.MAX_VALUE;
-        long negMin = negative.size() >= 2 ? negative.get(0) + negative.get(1) : Long.MAX_VALUE;
-
-        if (Math.abs(min) < Math.min(Math.abs(posMin), Math.abs(negMin))) {
-            sb.append(negative.get(nIdx)).append(" ").append(positive.get(pIdx));
-        } else if (Math.abs(posMin) < Math.abs(negMin)) {
-            sb.append(positive.get(0)).append(" ").append(positive.get(1));
-        } else {
-            sb.append(negative.get(1)).append(" ").append(negative.get(0));
-        }
-
+        sb.append(answer[0]).append(" ").append(answer[1]);
         System.out.println(sb);
     }
 }
